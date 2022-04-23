2f177a455034e69b39536efccf8c3e757b9c9aa7
mostlyclicks <carlos@mostlyclicks.com>
2022-04-23 17:50:25 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: `mostlyClicks-2022`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-G6PHXWZKR2"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
